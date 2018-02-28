4df50fb065322a3f6200e0315cbfb2a9d86230ce
dgalus <dawid_galus@wp.pl>
2018-02-28 15:22:53 +0100

    port numbers

--- tmp.py
+++ tmp.py
@@ -1,18 +1,40 @@
-import socket, struct, os, array
+import socket, struct, os, array, sys
 from scapy.all import ETH_P_ALL
 from scapy.all import select
 from scapy.all import MTU
- 
+from scapy.config import conf
+import ctypes
+import fcntl
+
+class ifreq(ctypes.Structure):
+    _fields_ = [("ifr_ifrn", ctypes.c_char*16),
+                ("ifr_flags", ctypes.c_short)]
+
+class FLAGS(object):
+    # linux/if_ether.h
+    ETH_P_ALL     = 0x0003 # all protocols
+    ETH_P_IP      = 0x0800 # IP only
+    # linux/if.h
+    IFF_PROMISC   = 0x100
+    # linux/sockios.h
+    SIOCGIFFLAGS  = 0x8913 # get the active flags
+    SIOCSIFFLAGS = 0x8914 # set the active flags
+
 class IPSniff:
-    def __init__(self, interface_name, on_ip_incoming, on_ip_outgoing):
+    def __init__(self, interface_name):#, on_ip_incoming, on_ip_outgoing):
         self.interface_name = interface_name
-        self.on_ip_incoming = on_ip_incoming
-        self.on_ip_outgoing = on_ip_outgoing
+        #self.on_ip_incoming = on_ip_incoming
+        #self.on_ip_outgoing = on_ip_outgoing
  
         # The raw in (listen) socket is a L2 raw socket that listens
         # for all packets going through a specific interface.
-        self.ins = socket.socket(
-            socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+        self.ins = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+        ifr = ifreq()
+        ifr.ifr_ifrn = bytes(interface_name, "utf-8")
+        fcntl.ioctl(self.ins, FLAGS.SIOCGIFFLAGS, ifr)
+        ifr.ifr_flags |= FLAGS.IFF_PROMISC
+        fcntl.ioctl(self.ins, FLAGS.SIOCGIFFLAGS, ifr)
+        self.ifr = ifr
         self.ins.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2**30)
         self.ins.bind((self.interface_name, ETH_P_ALL))
  
@@ -23,22 +45,33 @@ class IPSniff:
         iplen = fields[2]
         ip_src = payload[12:16]
         ip_dst = payload[16:20]
+        proto = payload[9]
         ip_frame = payload[0:iplen]
+        
+        if proto == 6 or proto == 17:
+            if dummy_hdrlen <= 5:
+                src_port = payload[20:22]
+                dest_port = payload[22:24]
+            else:
+                st_byte = dummy_hdrlen*4
+                src_port = payload[st_byte:st_byte+2]
+                dest_port = payload[st_byte+2:st_byte+4]
+            print("%s:%d -> %s:%d" % (socket.inet_ntoa(ip_src), struct.unpack('>H', src_port)[0], socket.inet_ntoa(ip_dst), struct.unpack('>H', dest_port)[0]))
  
-        if pkt_type == socket.PACKET_OUTGOING:
-            if self.on_ip_outgoing is not None:
-                self.on_ip_outgoing(ip_src, ip_dst, ip_frame)
-        else:
-            if self.on_ip_incoming is not None:
-                self.on_ip_incoming(ip_src, ip_dst, ip_frame)
+#        if pkt_type == socket.PACKET_OUTGOING:
+#            if self.on_ip_outgoing is not None:
+#                self.on_ip_outgoing(ip_src, ip_dst, ip_frame)
+#        else:
+#            if self.on_ip_incoming is not None:
+#                self.on_ip_incoming(ip_src, ip_dst, ip_frame)
  
     def recv(self):
         while True:
             pkt, sa_ll = self.ins.recvfrom(MTU)
-            if type == socket.PACKET_OUTGOING and self.on_ip_outgoing is None:
-                continue
-            elif self.on_ip_outgoing is None:
-                continue
+#            if type == socket.PACKET_OUTGOING and self.on_ip_outgoing is None:
+#                continue
+#            elif self.on_ip_outgoing is None:
+#                continue
             if len(pkt) <= 0:
                 break
             eth_header = struct.unpack("!6s6sH", pkt[0:14])
@@ -55,5 +88,5 @@ def test_incoming_callback(src, dst, frame):
 def test_outgoing_callback(src, dst, frame):
     print("outgoing - src=%s, dst=%s, frame len = %d" %(socket.inet_ntoa(src), socket.inet_ntoa(dst), len(frame)))
  
-ip_sniff = IPSniff('eno1', test_incoming_callback, test_outgoing_callback)
+ip_sniff = IPSniff(sys.argv[1])#, test_incoming_callback, test_outgoing_callback)
 ip_sniff.recv()
