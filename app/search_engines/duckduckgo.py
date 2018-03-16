c6f4be91ebd54b003756a63374f08b07483bd4d0
dgalus <dawid_galus@wp.pl>
2018-03-16 15:27:20 +0100

    duckduckgo - first

---
+++ app/search_engines/duckduckgo.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_position(domain):
+    r = requests.post('https://duckduckgo.com/html/', data={'q':domain})
+    soup = BeautifulSoup(r.text, 'html.parser')
+    elements = soup.find_all("a", class_="result__a")
+    for i in range(0, len(elements)):
+        if "//" + domain in str(elements[i]):
+            return i
+    return None
+    
+print(get_position('radio.opole.pl'))
