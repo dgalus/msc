6a794c60a344ffd557d59eabba9c1e7c3602efcc
avc <dawid_galus@wp.pl>
2018-03-18 20:04:56 +0100

    tcp checksum

--- app/scan/tcp.py
+++ app/scan/tcp.py
@@ -1,13 +1,26 @@
 import socket
+import struct
 
-def tcp_connect_scan(hostname):
+def checksum(msg):
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8 )
+        s = s + w
+    s = (s>>16) + (s & 0xffff);
+    s = s + (s >> 16);
+    s = ~s & 0xffff
+    return s
+
+def tcp_connect_scan(dest_ip):
     pass
 
-def tcp_syn_scan(hostname):
+def tcp_syn_scan(ip):
     pass
 
-def tcp_ack_scan(hostname):
+def tcp_ack_scan(ip):
     pass
 
-def tcp_fin_scan(hostname):
+def tcp_fin_scan(ip):
     pass
+
+tcp_connect_scan('192.168.1.1')
