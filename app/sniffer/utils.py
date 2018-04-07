8143693e08a60b3e993f25b0eddca5d0c7d286e9
avc <dawid_galus@wp.pl>
2018-04-07 13:00:36 +0200

    is_local_address

--- app/sniffer/utils.py
+++ app/sniffer/utils.py
@@ -1,4 +1,10 @@
 import ipaddress
 
 def is_local_address(ip):
-    pass
+    if ip >= 0x0A000000 and ip <= 0x0AFFFFFF:
+        return True
+    if ip >= 0xAC100000 and ip <= 0xAC1FFFFF:
+        return True
+    if ip >= 0xC0A80000 and ip <= 0xC0A8FFFF:
+        return True
+    return False
