3176c0c639ec53a8d5307981957fa763046732fe
dgalus <dawid_galus@wp.pl>
2018-04-12 12:22:46 +0200

    new args

--- app/analysis/domain.py
+++ app/analysis/domain.py
@@ -1,5 +1,13 @@
+import socket
 from ..geolocation import GeoLocation
+from ..database import RethinkDB
 
 def analyze(domain):
     name = domain
     country = GeoLocation.get_country_by_name(domain)
+    ip = __get_ip_by_domain(domain)
+    db = RethinkDB()
+    
+    
+def __get_ip_by_domain(domain):
+    return socket.gethostbyname(domain)
--- run.py
+++ run.py
@@ -8,6 +8,8 @@ from app.geolocation import GeoLocation
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--clear_db", action="store_true", help="Clear database")
+parser.add_argument("-d", "--daemon", action="store_true", help="Run as daemon")
+parser.add_argument("-g", "--initialize_geolocation", action="store_true", help="(Re-)Initialize geolocation module")
 parser.add_argument("-u", "--initialize_unsafe", action="store_true", help="Initialize a new set of unsafe URLs, domains and IPs")
 
 args = parser.parse_args()
@@ -15,8 +17,11 @@ args = parser.parse_args()
 if args.clear_db:
     pass
 if args.initialize_unsafe:
+    initialize_unsafe_connections_list()
+if args.initialize_geolocation:
+    GeoLocation.initialize()
+if args.daemon:
     pass
 
 
-initialize_unsafe_connections_list()
-GeoLocation.initialize()
+
