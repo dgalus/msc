4b0c2c7f63a9d453a2f45e9f47c280f2ea5fdb9b
avc <dawid_galus@wp.pl>
2018-04-15 13:03:03 +0200

    main()

--- run.py
+++ run.py
@@ -7,27 +7,28 @@ from app.geolocation import GeoLocation
 from app.database import RethinkDB
 from app.sniffer import Counters
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--clear_db", action="store_true", help="Clear database")
-parser.add_argument("-d", "--daemon", action="store_true", help="Run as daemon")
-parser.add_argument("-g", "--initialize_geolocation", action="store_true", help="(Re-)Initialize geolocation module")
-parser.add_argument("-u", "--initialize_unsafe", action="store_true", help="Initialize a new set of unsafe URLs, domains and IPs")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--clear_db", action="store_true", help="Clear database")
+    parser.add_argument("-d", "--daemon", action="store_true", help="Run as daemon")
+    parser.add_argument("-g", "--initialize_geolocation", action="store_true", help="(Re-)Initialize geolocation module")
+    parser.add_argument("-u", "--initialize_unsafe", action="store_true", help="Initialize a new set of unsafe URLs, domains and IPs")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if args.clear_db:
-    RethinkDB().clear_db()
-if args.initialize_unsafe:
-    initialize_unsafe_connections_list()
-if args.initialize_geolocation:
-    GeoLocation.initialize()
-if args.daemon:
-    c = Counters()
-    schedule.every(1).minutes.do(c.store_counters())
-    
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
+    if args.clear_db:
+        RethinkDB().clear_db()
+    if args.initialize_unsafe:
+        initialize_unsafe_connections_list()
+    if args.initialize_geolocation:
+        GeoLocation.initialize()
+    if args.daemon:
+        c = Counters()
+        schedule.every(1).minutes.do(c.store_counters())
 
+        while 1:
+            schedule.run_pending()
+            time.sleep(1)
 
-
+if __name__ == '__main__':
+    main()
