828cd495529963e9a37a7f3ff5b1333f14e0bb3b
avc <dawid_galus@wp.pl>
2018-04-16 21:17:35 +0200

    delete unused responses

--- app/scan/__init__.py
+++ app/scan/__init__.py
@@ -4,26 +4,6 @@ class PingScanResponse(Enum):
     HOST_AVAILABLE = 1
     HOST_UNAVAILABLE = 2
 
-class TCPConnectScanResponse(Enum):
-    PORT_OPEN = 1
-    PORT_CLOSED = 2
-
-class TCPSynScanResponse(Enum):
-    PORT_OPEN = 1
-    PORT_CLOSED = 2
-
-class TCPFinScanResponse(Enum):
-    PORT_OPEN_OR_FILTERED = 1
-    PORT_CLOSED = 2
-
-class TCPAckScanResponse(Enum):
-    PORT_SECURED = 1
-    PORT_NOT_SECURED = 2
-
-class UDPScanResponse(Enum):
-    PORT_OPEN_OR_FILTERED = 1
-    PORT_CLOSED = 2
-
 from .ping import ping
 from .tcp import tcp_ack_scan, tcp_connect_scan, tcp_fin_scan, tcp_syn_scan
 from .udp import udp_scan
