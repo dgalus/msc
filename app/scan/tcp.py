326ba6f5c59ab9b2bd3a64244eb1507096c65510
avc <dawid_galus@wp.pl>
2018-04-21 13:22:14 +0200

    no tcp_ack_scan

--- app/scan/tcp.py
+++ app/scan/tcp.py
@@ -23,17 +23,6 @@ def tcp_syn_scan(ip):
             open_ports.append(int(px.get('portid')))
     return open_ports
 
-def tcp_ack_scan(ip):
-    nm = NmapProcess(ip, options="-sA -T4 -p 1-65535")
-    nm.run()
-    soup = BeautifulSoup(nm.stdout, "lxml")
-    ports_xml = soup.find('host').find('ports').find_all('port')
-    open_ports = []
-    for px in ports_xml:
-        if px.find('state').get('state') == 'open':
-            open_ports.append(int(px.get('portid')))
-    return open_ports    
-
 def tcp_fin_scan(ip):
     nm = NmapProcess(ip, options="-sF -T4 -p 1-65535")
     nm.run()
