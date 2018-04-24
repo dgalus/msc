bf6a0cca97acac81e70799cf253dedab320cd2b6
avc <dawid_galus@wp.pl>
2018-04-24 09:52:53 +0200

    install

--- sniffer/install.py
+++ sniffer/install.py
@@ -1,19 +1,17 @@
 import datetime
-from sqlalchemy import Table, Column, Integer, String, ForeignKey, create_engine, MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine  
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.orm import sessionmaker
 
-def connect(user, password, db, host='127.0.0.1', port=5432):
-	url = 'postgresql://{}:{}@{}:{}/{}'
-	url = ur.format(user, password, host, port, db)
-	con = sqlalchemy.create_engine(url, client_encoding='utf8')
-	meta = sqlalchemy.MetaData(bind=con, reflect=True)
-	return con, meta
+db_string = "postgres://sniffer:sniffer@127.0.0.1:5432/sniffer"
 
-Base = declarative_base()
+db = create_engine(db_string)  
+base = declarative_base()
 
-class Counter(Base):
+class Counter(base):
 	__tablename__ = 'counters'
-	id = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True, nullable=False)
 	timestamp = Column(DateTime, nullable=False)
 	tcp_syn = Column(Integer, nullable=False)
 	tcp_ack = Column(Integer, nullable=False)
@@ -32,7 +30,6 @@ class Counter(Base):
 	l2_frames = Column(Integer, nullable=False)
 	l3_frames = Column(Integer, nullable=False)
 	l4_frames = Column(Integer, nullable=False)
-	
 	
 	def __init__(self, tcp_syn, tcp_ack, tcp_synack, tcp_psh, tcp_rst, tcp_fin, tcp, 
 		ip, arp, udp, icmp, l2_traffic, l3_traffic, l4_traffic, l2_frames, l3_frames, l4_frames):
@@ -55,3 +52,7 @@ class Counter(Base):
 		self.l3_frames = l3_frames
 		self.l4_frames = l4_frames
 		
+
+Session = sessionmaker(db)  
+session = Session()
+base.metadata.create_all(db)
