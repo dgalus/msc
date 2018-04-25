87da153f05200f50a281c58c53c9529cfc05176e
avc <dawid_galus@wp.pl>
2018-04-25 13:29:53 +0200

    delete unused elements

--- app/sniffer/__init__.py
+++
@@ -1,8 +0,0 @@
-from .enums import Direction
-from .tcp_session import TCPSession
-from .tcp_segment import TCPSegment, TCPSegmentDirection
-from .counters import Counters
-from .udp_session import *
-from .sniffer import Sniffer
-from .utils import *
-
--- app/sniffer/enums.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class Direction(Enum):
-    FROM_SRC_TO_DST = 1
-    FROM_DST_TO_SRC = 2
