c6340dd658d735fbbbdfbd6ce2c3f7333e747d61
avc <dawid_galus@wp.pl>
2018-05-16 01:13:54 +0200

    anomaly detection

--- tests/sigmoidal.py
+++ tests/sigmoidal.py
@@ -1,11 +1,9 @@
 from statistics import *
-from sklearn.cluster import KMeans
-from sklearn import datasets
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-TCP_SYN_FILE = 'tcp_syn_trunc.dat'
+TCP_SYN_FILE = 'tcp_syn.dat'
 
 def read_file(filename):
     arr = []
@@ -16,66 +14,76 @@ def read_file(filename):
 
 traffic = read_file(TCP_SYN_FILE)
 
-def initial_trend(series, season_length):
-    s = 0.0
-    for i in range(season_length):
-        s += float(series[i+season_length] - series[i])/season_length
-    return s/season_length
+def simple_exp_smoothing(series, alpha):
+    result = [series[0]]
+    for n in range(1, len(series)):
+        result.append(alpha*series[n] + (1-alpha)*result[n-1])
+    return result[-1]
 
-def initial_seasonal_components(series, season_length):
-    seasonals = {}
-    season_averages = []
-    n_seasons = int(len(series)/season_length)
-    for j in range(n_seasons):
-        season_averages.append(sum(series[season_length*j:season_length*j+season_length])/float(season_length))
-    for i in range(season_length):
-        sum_of_vals_avg = 0.0
-        for j in range(n_seasons):
-            sum_of_vals_avg += series[season_length*j+i]-season_averages[j]
-        seasonals[i] = sum_of_vals_avg/n_seasons
-    return seasonals
+def calculate_trend():
+    pass
 
-def holt_winters_forecast(series, season_length, alpha, beta, gamma, n_preds):
-    result = []
-    seasonals = initial_seasonal_components(series, season_length)
-    for i in range(len(series) + n_preds):
-        if i == 0:
-            smooth = series[0]
-            trend = initial_trend(series, season_length)
-            result.append(series[0])
-            continue
-        if i >= len(series):
-            m = i-len(series)+1
-            result.append((smooth+m*trend) + seasonals[i%season_length])
-        else:
-            val = series[i]
-            last_smooth = smooth
-            smooth = alpha*(val-seasonals[i%season_length]) + (1-alpha)*(smooth+trend)
-            trend = beta*(smooth-last_smooth)+(1-beta)*trend
-            seasonals[i%season_length] = gamma*(val-smooth)+(1-gamma)*seasonals[i%season_length]
-            result.append(smooth+trend+seasonals[i%season_length])
-    return result
+def get_biggest_increase_in_window(series):
+    inc = 0
+    for i in range(len(series)):
+        if i < len(series)-1:
+            if series[i+1] - series[i] > inc:
+                inc = series[i+1] - series[i]
+    return inc
 
+def get_biggest_decrease_in_window(series):
+    inc = 0
+    for i in range(len(series)):
+        if i < len(series)-1:
+            if series[i] - series[i+1] > inc:
+                inc = series[i] - series[i+1]
+    return inc
+    
 
-def holt_long():
-    aver = []
+def sigmoid(a, b, x):
+    try:
+        return 1./(1+math.exp(-1.*a*(x-b)))
+    except:
+        return 0
+
+def sigmoidal():
+    res = []
     st = traffic[4320:-1440]
-    m = mean(st)
-    res = holt_winters_forecast(st, 1440, 0.99, 0, 0.1, 1440)
-    aver = res[-1440:]
-    aver = res[-1440:]
-    l_ci = []
-    h_ci = []
-    for i in aver:
-        l_ci.append(i - m)
-        h_ci.append(i + m)
-    _, ax = plt.subplots()
-    ax.plot(range(0, 1440), traffic[-1440:], linewidth=0.7, color='#df9292', alpha=1, label="Rzeczywisty ruch")
-    ax.plot(range(0, 1440), aver, linewidth=0.7, color = '#539caf', alpha=1, label="Prognozowany ruch")
-    ax.fill_between(range(0, 1440), l_ci, h_ci, color = '#539caf', alpha = 0.4, label="Przedział ufności")
-    ax.set_title("Prognozowanie Holta-Wintersa dla TCP SYN")
-    ax.set_xlabel("Czas")
-    ax.set_ylabel("Ilość segmentów TCP SYN")
-    ax.legend(loc='best')
-    plt.savefig('hw.svg', format="svg")
-    plt.clf()
+    window_size = 21
+    season_length = 1440
+    alpha = 0.9
+    sigmoidal_threshold = 0.9
+    elements = []
+    sigm_res = []
+    # prognoza na sezon
+    for i in range(season_length):
+        st = traffic[4320:-2880+i]
+        differences_increase = []
+        # jesli sa dane do prognozowania
+        if len(st) > season_length:
+            start_item = len(st) % season_length
+            end_item = len(st) - 1
+            while True:
+                if start_item-int(window_size/2) < 0:
+                    differences_increase.append(get_biggest_increase_in_window(st[0:start_item+int(window_size/2)]))
+                else:
+                    differences_increase.append(get_biggest_increase_in_window(st[start_item-int(window_size/2):start_item+int(window_size/2)]))
+                start_item += season_length
+                if start_item > end_item:
+                    sigm = stdev(st[-1440:])
+                    elements.append(st[-1] + simple_exp_smoothing(differences_increase, alpha))
+                    sigm_res.append(sigmoid(1/sigm, st[-1] + simple_exp_smoothing(differences_increase, 0.9), traffic[-2880+i+1]))
+                    if sigm_res[-1] > sigmoidal_threshold:
+                        print("ALERT")
+                    print("zmierzone = " + str(traffic[-2880+i+1]) + "    |    prognoza = " + str(elements[-1]) + "    |     sigmoidal = " + str(sigm_res[-1]))
+                    break
+    #_, ax = plt.subplots()
+    #ax.plot(range(0, 1440), traffic[-2880:-1440], linewidth=0.7, color='#df9292', alpha=1, label="Rzeczywisty ruch")
+    #ax.plot(range(0, 1440), elements, linewidth=0.7, color = '#539caf', alpha=1, label="Prognozowany ruch")
+    #ax.set_xlabel("Czas")
+    #ax.set_ylabel("Ilość segmentów TCP SYN")
+    #ax.legend(loc='best')
+    #plt.show()
+    #plt.clf()
+        
+sigmoidal()
