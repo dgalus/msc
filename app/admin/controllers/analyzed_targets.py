408d8f0416996dabb9f024fd6fe5bf16ab49a414
dgalus <dawid_galus@wp.pl>
2018-05-16 15:32:09 +0200

    website_security_rate - beginning

--- app/admin/controllers/analyzed_targets.py
+++ app/admin/controllers/analyzed_targets.py
@@ -12,7 +12,20 @@ def blocked_domains():
 
 @app.route('/analyzed_targets/website_security_rate', methods=['GET'])
 def website_security_rate():
-    return render_template('analyzed_targets/website_security_rate.html')
+    sites_db = db.session.query(AnalyzedHTTPSite).filter(AnalyzedHTTPSite.analyze_timestamp != None).all()
+    sites = []
+    for s in sites_db:
+        d = {}
+        d['domain'] = s.domain
+        d['url'] = s.urls.split()
+        d['ip'] = s.ip
+        d['analyze_timestamp'] = s.analyze_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        d['geolocation'] = s.geolocation
+        d['google_rank'] = s.google_rank
+        d['duckduckgo_rank'] = s.duckduckgo_rank
+        d['last_visited'] = s.last_visited.strftime("%Y-%m-%d %H:%M:%S")
+        #d['admin_safe'] =
+    return render_template('analyzed_targets/website_security_rate.html', sites=sites)
 
 @app.route('/analyzed_targets/geolocation', methods=['GET'])
 def geolocation():
