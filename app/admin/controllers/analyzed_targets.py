5b3d0536c4b386ce0c36d127c2f659a60b3a18b0
dgalus <dawid_galus@wp.pl>
2018-05-17 13:42:51 +0200

    website security rate in web panel

--- app/admin/controllers/analyzed_targets.py
+++ app/admin/controllers/analyzed_targets.py
@@ -12,19 +12,26 @@ def blocked_domains():
 
 @app.route('/analyzed_targets/website_security_rate', methods=['GET'])
 def website_security_rate():
-    sites_db = db.session.query(AnalyzedHTTPSite).filter(AnalyzedHTTPSite.analyze_timestamp != None).all()
+    sites_db = db.session.query(AnalyzedHTTPSite).filter(AnalyzedHTTPSite.analyze_timestamp != None).order_by(AnalyzedHTTPSite.domain.asc()).all()
     sites = []
     for s in sites_db:
         d = {}
         d['domain'] = s.domain
-        d['url'] = s.urls.split()
+        d['urls'] = s.urls.split()
         d['ip'] = s.ip
-        d['analyze_timestamp'] = s.analyze_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        d['analyze_timestamp'] = s.analyze_timestamp
         d['geolocation'] = s.geolocation
         d['google_rank'] = s.google_rank
         d['duckduckgo_rank'] = s.duckduckgo_rank
-        d['last_visited'] = s.last_visited.strftime("%Y-%m-%d %H:%M:%S")
-        #d['admin_safe'] =
+        d['last_visited'] = s.last_visited
+        d['is_admin_safe'] = s.is_admin_safe
+        d['https'] = s.https
+        d['hsts'] = s.hsts
+        d['cors'] = s.cors
+        d['bayes_safe'] = s.bayes_safe
+        d['is_blacklisted'] = s.is_blacklisted
+        d['rank'] = s.rank
+        sites.append(d)
     return render_template('analyzed_targets/website_security_rate.html', sites=sites)
 
 @app.route('/analyzed_targets/geolocation', methods=['GET'])
