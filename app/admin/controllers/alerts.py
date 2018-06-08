2f8770b8fb65b611b90d0c7d7834e92efc7f3cab
dgalus <dawid_galus@wp.pl>
2018-06-08 19:38:29 +0200

    mark alert as viewed

--- app/admin/controllers/alerts.py
+++ app/admin/controllers/alerts.py
@@ -6,9 +6,21 @@ def alerts():
     alerts = []
     for a in alerts_db:
         d = {}
+        d['id'] = a.id
         d['timestamp'] = a.timestamp.strftime("%Y-%m-%d %H:%M:%S")
         d['alert_type'] = str(AlertType(a.alert_type))
         d['description'] = a.description
         d['rank'] = str(a.rank)
         alerts.append(d)
     return render_template('alerts/index.html', alerts=alerts)
+
+@app.route('/mark_alert_as_viewed', methods=['POST'])
+def mark_alert_as_viewed():
+    try:
+        alert_id = int(request.form['alert_id'])
+        alert = db.session.query(Alert).filter_by(id=alert_id).first()
+        alert.admin_delete = True
+        db.session.commit()
+    except:
+        pass
+    return redirect('/alerts')
