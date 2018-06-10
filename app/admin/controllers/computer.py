a5c20dc824835a19d8f87e1f1b84e6bf8d2a8e8e
dgalus <dawid_galus@wp.pl>
2018-06-10 22:20:31 +0200

    show computers

--- app/admin/controllers/computer.py
+++ app/admin/controllers/computer.py
@@ -5,6 +5,26 @@ def aliases():
     computers = db.session.query(Computer).all()
     return render_template('computer/aliases.html', computers=computers)
 
+@app.route('/computer/show', methods=['GET'])
+def show():
+    comps = db.session.query(Computer).all()
+    computers = []
+    for c in comps:
+        d = {}
+        d["alias"] = c.alias
+        d['ip'] = c.ip
+        if c.geolocations:
+            d['geolocations'] = str(json.loads(c.geolocations)["geolocations"])
+        d['open_ports'] = c.open_ports
+        d['closed_ports'] = c.closed_ports
+        d['last_active'] = c.last_active
+        d['last_ping'] = c.last_ping_response_timestamp
+        d['last_port_scan'] = c.last_port_scan
+        if c.most_connected_ports:
+            d['most_connected'] = str(json.loads(c.most_connected_ports)["ports"])
+        computers.append(d)
+    return render_template('computer/show.html', computers=computers)
+
 @app.route('/computer/most_visited', methods=['GET'])
 def most_visited():
     return render_template('computer/most_visited.html')
